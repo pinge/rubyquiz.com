0bf21718301ede59b4537050bbb5142358371913
Nuno Pinge <nuno@pinge.org>
2012-06-16 08:58:03 +0100

    finished specs for quiz #1

--- rubyquiz/1_the_solitaire_cipher/solitaire.rb
+++ rubyquiz/1_the_solitaire_cipher/solitaire.rb
@@ -21,7 +21,7 @@ module Rubyquiz
       def triple_cut
         top_joker, bottom_joker = @deck.index('A') < @deck.index('B') ? ['A','B'] : ['B','A']
         above_top_joker = top_joker == @deck.first ? [] : @deck[0..@deck.index(top_joker)-1]
-        below_bottom_joker = bottom_joker == @deck.last ? [] : @deck[@deck.index(top_joker)+1..-1]
+        below_bottom_joker = bottom_joker == @deck.last ? [] : @deck[@deck.index(bottom_joker)+1..-1]
         @deck = below_bottom_joker + @deck[@deck.index(top_joker)..@deck.index(bottom_joker)] + above_top_joker
         self
       end
@@ -31,9 +31,13 @@ module Rubyquiz
       end
 
       def output_letter
-        card = @deck[@deck.first]
+        card = @deck[['A','B'].include?(@deck.first) ? 53 : @deck.first]
         return nil if ['A','B'].include?(card)
         return (64 + card % 26).chr
+      end
+
+      def to_s
+        @deck.inject(""){ |output,card| output << "#{card} " }
       end
 
       private
@@ -49,22 +53,88 @@ module Rubyquiz
 
     end
 
+    module Encryption
+
+      def generate_keystream_letter(deck)
+        output_letter = nil
+        while output_letter.nil? do
+          deck.move_A
+          deck.move_B
+          deck.triple_cut
+          deck.count_cut
+          output_letter = deck.output_letter
+        end
+        output_letter
+      end
+
+      def generate_keystreamed_message(message)
+        deck = Deck.new
+        message.split("").inject(""){ |output,char| output << (char == " " ? " " : self.generate_keystream_letter(deck)) }
+      end
+
+      def letter_to_number(letter)
+        letter.ord - "A".ord + 1
+      end
+
+      def letters_to_numbers(message)
+        message.split("").inject([]){ |output,char| output << (char == " " ? nil : self.letter_to_number(char)) }
+      end
+
+      def number_to_letter(number)
+        ("A".ord + number.to_i - 1).chr
+      end
+
+      def numbers_to_letters(message)
+        message.inject(""){ |output,number| output << (number.nil? ? " " : self.number_to_letter(number)) }
+      end
+
+      def merge(message, keystreamed_message, &merger)
+        message.each_with_index.inject([]) do |output, (value,index)|
+          output << (value.nil? ? nil : merger.call(value, keystreamed_message[index]))
+        end
+      end
+
+    end
+
     class Encrypt
 
-      # step 1
+      extend Encryption
+
       def self.discard_non_az_and_upcase(message)
         message.upcase.gsub(/[^A-Z]/,'')
       end
 
-      # split the message into five character groups, using Xs to pad the last group, if needed
       def self.split_and_pad(message)
         pad_remainder = message.size % 5
         message += ("X" * (5 - pad_remainder)) unless pad_remainder == 0
         message.scan(/.{5}/).join(" ")
       end
 
-      def self.step_1(message)
+      def self.normalize(message)
         self.split_and_pad(self.discard_non_az_and_upcase(message))
+      end
+
+      def self.run(message)
+        normalized_message = self.normalize(message)
+        keystreamed_message = self.generate_keystreamed_message(normalized_message)
+        normalized_message_with_numbers = self.letters_to_numbers(normalized_message)
+        keystreamed_message_with_numbers = self.letters_to_numbers(keystreamed_message)
+        output = self.merge(keystreamed_message_with_numbers, normalized_message_with_numbers){ |v1, v2| t = v1 + v2; t / 26 > 0 ? t % 26 : t }
+        self.numbers_to_letters(output)
+      end
+
+    end
+
+    class Decrypt
+
+      extend Encryption
+
+      def self.run(message)
+        keystreamed_message = self.generate_keystreamed_message(message)
+        message_with_numbers = self.letters_to_numbers(message)
+        keystreamed_message_with_numbers = self.letters_to_numbers(keystreamed_message)
+        output = self.merge(message_with_numbers, keystreamed_message_with_numbers){ |v1, v2| t = v1 - v2; t > 0 ? t : t + 26 }
+        self.numbers_to_letters(output)
       end
 
     end
--- spec/quizzes/1_the_solitaire_cipher_spec.rb
+++ spec/quizzes/1_the_solitaire_cipher_spec.rb
@@ -95,12 +95,60 @@ module Rubyquiz
 
         let(:deck_of_cards){ Deck.new }
 
-        it "should be able to move all cards above the top joker to below the bottom joker and vice versa" do
+        it "should have a method called triple_cut" do
           deck_of_cards.should respond_to(:triple_cut)
-          deck_of_cards.move_A
-          deck_of_cards.move_B
-          deck_of_cards.triple_cut
-          deck_of_cards.instance_variable_get(:@deck).should == ['B'] + (2..52).to_a + ['A',1]
+        end
+
+        context "should be able to move all cards above the top joker to below the bottom joker and vice versa" do
+
+          it "when joker A is at the bottom of the deck and there are cards between the two jokers" do
+            deck_of_cards.instance_variable_set(:@deck, (1..10).to_a + ['B'] + (11..52).to_a + ['A'])
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == ['B'] + (11..52).to_a + ['A'] + (1..10).to_a
+          end
+
+          it "when joker B is at the bottom of the deck and there are cards between the two jokers" do
+            deck_of_cards.instance_variable_set(:@deck, (1..10).to_a + ['A'] + (11..52).to_a + ['B'])
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == ['A'] + (11..52).to_a + ['B'] + (1..10).to_a
+          end
+
+          it "when joker A is at the top of the deck and there are cards between the two jokers" do
+            deck_of_cards.instance_variable_set(:@deck, ['A'] + (1..10).to_a + ['B'] + (11..52).to_a)
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == (11..52).to_a + ['A'] + (1..10).to_a + ['B']
+          end
+
+          it "when joker B is at the top of the deck and there are cards between the two jokers" do
+            deck_of_cards.instance_variable_set(:@deck, ['B'] + (1..12).to_a + ['A'] + (13..52).to_a)
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == (13..52).to_a + ['B'] + (1..12).to_a + ['A']
+          end
+
+          it "when joker A is above joker B, the two jokers are together and are in the middle of the deck" do
+            deck_of_cards.instance_variable_set(:@deck, (1..20).to_a + ['A','B'] + (21..52).to_a)
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == (21..52).to_a + ['A','B'] + (1..20).to_a
+          end
+
+          it "when joker B is above joker A, the two jokers are together and are in the middle of the deck" do
+            deck_of_cards.instance_variable_set(:@deck, (1..30).to_a + ['B','A'] + (31..52).to_a)
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == (31..52).to_a + ['B','A'] + (1..30).to_a
+          end
+
+          it "when joker A is above joker B, there are cards between the two jokers and the two jokers are in the middle of the deck" do
+            deck_of_cards.instance_variable_set(:@deck, (1..20).to_a + ['A'] + (21..25).to_a + ['B'] + (26..52).to_a)
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == (26..52).to_a + ['A'] + (21..25).to_a + ['B'] + (1..20).to_a
+          end
+
+          it "when joker B is above joker A, there are cards between the two jokers and the two jokers are in the middle of the deck" do
+            deck_of_cards.instance_variable_set(:@deck, (1..10).to_a + ['B'] + (11..41).to_a + ['A'] + (42..52).to_a)
+            deck_of_cards.triple_cut
+            deck_of_cards.instance_variable_get(:@deck).should == (42..52).to_a + ['B'] + (11..41).to_a + ['A'] + (1..10).to_a
+          end
+
         end
 
       end
@@ -145,6 +193,61 @@ module Rubyquiz
 
     end
 
+    describe "Rubyquiz::Solitaire::Encryption module" do
+
+      it "should enable a generic Encryption/Decryption class to extend it" do
+
+        class Crypt
+          extend Rubyquiz::Solitaire::Encryption
+        end
+
+      end
+
+      context "a Crypt class that extends the module" do
+
+        let(:crypt){ class Crypt; extend Rubyquiz::Solitaire::Encryption end }
+
+        it "should generate a keystream letter for any given letter in a message" do
+          deck_of_cards = Deck.new
+          crypt.should respond_to(:generate_keystream_letter)
+          crypt.generate_keystream_letter(deck_of_cards).should == "D"
+          crypt.generate_keystream_letter(deck_of_cards).should == "W"
+        end
+
+        it "should generate a keystreamed message for any given message" do
+          crypt.should respond_to(:generate_keystreamed_message)
+          crypt.generate_keystreamed_message("CODEI NRUBY LIVEL ONGER").should == "DWJXH YRFDG TMSHP UURXJ"
+        end
+
+        it "should convert any letter (A-Z) to a number (1-26), C to 3, W to 15, etc" do
+          crypt.should respond_to(:letter_to_number)
+          ("A".."Z").to_a.each_with_index do |letter,index|
+            crypt.letter_to_number(letter).should == index+1
+          end
+        end
+
+        it "should convert a message of letter to numbers" do
+          crypt.should respond_to(:letters_to_numbers)
+          crypt.letters_to_numbers("CODEI NRUBY LIVEL ONGER").should == [3,15,4,5,9,nil,14,18,21,2,25,nil,12,9,22,5,12,nil,15,14,7,5,18]
+        end
+
+        it "should convert a number (1-26) to a letter (A-Z), 3 to C, 15 to W, etc" do
+          crypt.should respond_to(:number_to_letter)
+          letters = ("A".."Z").to_a
+          (1..26).to_a.each_with_index do |number,index|
+            crypt.number_to_letter(number).should == letters[index]
+          end
+        end
+
+        it "should convert a message of numbers to letters" do
+          crypt.should respond_to(:numbers_to_letters)
+          crypt.numbers_to_letters([3,15,4,5,9,nil,14,18,21,2,25,nil,12,9,22,5,12,nil,15,14,7,5,18]).should == "CODEI NRUBY LIVEL ONGER"
+        end
+
+      end
+
+    end
+
     describe Encrypt do
 
       let(:encrypt){ Rubyquiz::Solitaire::Encrypt }
@@ -166,9 +269,152 @@ module Rubyquiz
           encrypt.split_and_pad("CODEINRUBYLIVELONGER").should == "CODEI NRUBY LIVEL ONGER"
         end
 
+        it "should have a 'normalize' method to return the step result" do
+          encrypt.should respond_to(:normalize)
+          encrypt.normalize(message).should == "CODEI NRUBY LIVEL ONGER"
+        end
+
+      end
+
+      context "2. Use Solitaire to generate a keystream letter for each letter in the message." do
+
         it "should have a method to return the step result" do
-          encrypt.should respond_to(:step_1)
-          encrypt.step_1(message).should == "CODEI NRUBY LIVEL ONGER"
+          encrypt.should respond_to(:generate_keystreamed_message)
+          encrypt.generate_keystreamed_message("CODEI NRUBY LIVEL ONGER").should == "DWJXH YRFDG TMSHP UURXJ"
+        end
+
+      end
+
+      context "3. Convert the message from step 1. into numbers, A = 1, B = 2, etc.." do
+
+        it "should have a method to return the step result" do
+          encrypt.should respond_to(:letters_to_numbers)
+          encrypt.letters_to_numbers("CODEI NRUBY LIVEL ONGER").should == [3,15,4,5,9,nil,14,18,21,2,25,nil,12,9,22,5,12,nil,15,14,7,5,18]
+        end
+
+      end
+
+      context "4. Convert the keystream letters from step 2. using the same method" do
+
+        it "should have a method to return the step result" do
+          encrypt.letters_to_numbers("DWJXH YRFDG TMSHP UURXJ").should == [4,23,10,24,8,nil,25,18,6,4,7,nil,20,13,19,8,16,nil,21,21,18,24,10]
+        end
+
+      end
+
+      context "5. Add the message numbers from step 3 to the keystream numbers from step 4 and subtract 26 from the
+                  result if it is greater than 26. For example, 6 + 10 = 16 as expected, but 26 + 1 = 1 (27 - 26):" do
+
+        it "should have a method to return the step result" do
+          encrypt.should respond_to(:merge)
+          encrypt.merge([3,15,4,5,9,nil,14,18,21,2,25,nil,12,9,22,5,12,nil,15,14,7,5,18],
+                        [4,23,10,24,8,nil,25,18,6,4,7,nil,20,13,19,8,16,nil,21,21,18,24,10]){
+              |v1, v2| t = v1 + v2; t / 26 > 0 ? t % 26 : t
+          }.should == [7,12,14,3,17,nil,13,10,1,6,6,nil,6,22,15,13,2,nil,10,9,25,3,2]
+        end
+
+      end
+
+      context "6. Convert the numbers from step 5 back to letters" do
+
+        it "should have a method to return the step result" do
+          encrypt.should respond_to(:numbers_to_letters)
+          encrypt.numbers_to_letters([7,12,14,3,17,nil,13,10,1,6,6,nil,6,22,15,13,2,nil,10,9,25,3,2]).should == "GLNCQ MJAFF FVOMB JIYCB"
+        end
+
+      end
+
+      context "additional messages to encrypt" do
+
+        {
+            "Code in Ruby, live longer!" => "GLNCQ MJAFF FVOMB JIYCB"
+        }.each do |decrypted_message, encrypted_message|
+
+          it "should be able to encrypt the message: #{decrypted_message}" do
+
+            encrypt.run(decrypted_message).should == encrypted_message
+
+          end
+
+        end
+
+      end
+
+    end
+
+    describe Decrypt do
+
+      let(:decrypt){ Rubyquiz::Solitaire::Decrypt }
+      let(:message){ "GLNCQ MJAFF FVOMB JIYCB" }
+      let(:message_after_step_2){  }
+      let(:message_after_step_3){  }
+
+      context "1. Use Solitaire to generate a keystream letter for each letter in the message to be decoded. Again, I
+                  detail this process below, but sender and receiver use the same key and will get the same letters:" do
+
+        it "should have a method to return the step result" do
+          decrypt.should respond_to(:generate_keystreamed_message)
+          decrypt.generate_keystreamed_message(message).should == "DWJXH YRFDG TMSHP UURXJ"
+        end
+
+      end
+
+      context "2. Convert the message to be decoded to numbers" do
+
+        it "should have a method to return the step result" do
+          decrypt.should respond_to(:letters_to_numbers)
+          decrypt.letters_to_numbers(message).should == [7, 12, 14, 3, 17, nil, 13, 10, 1, 6, 6, nil, 6, 22, 15, 13, 2, nil, 10, 9, 25, 3, 2]
+        end
+
+      end
+
+      context "3. Convert the keystream letters from step 1 to numbers" do
+
+        it "should have a method to return the step result" do
+          decrypt.should respond_to(:letters_to_numbers)
+          decrypt.letters_to_numbers("DWJXH YRFDG TMSHP UURXJ").should == [4, 23, 10, 24, 8, nil, 25, 18, 6, 4, 7, nil, 20, 13, 19, 8, 16, nil, 21, 21, 18, 24, 10]
+        end
+
+      end
+
+      context "4. Subtract the keystream numbers from step 3 from the message numbers from step 2. If the message number
+                  is less than or equal to the keystream number, add 26 to the message number before subtracting.
+                  For example, 22 - 1 = 21 as expected, but 1 - 22 = 5 (27 - 22)" do
+
+        it "should have a method to return the step result" do
+          decrypt.should respond_to(:merge)
+          decrypt.merge([7, 12, 14, 3, 17, nil, 13, 10, 1, 6, 6, nil, 6, 22, 15, 13, 2, nil, 10, 9, 25, 3, 2],
+                        [4, 23, 10, 24, 8, nil, 25, 18, 6, 4, 7, nil, 20, 13, 19, 8, 16, nil, 21, 21, 18, 24, 10]){
+              |v1, v2| t = v1 - v2; t > 0 ? t : t + 26
+          }.should == [3, 15, 4, 5, 9, nil, 14, 18, 21, 2, 25, nil, 12, 9, 22, 5, 12, nil, 15, 14, 7, 5, 18]
+        end
+
+      end
+
+      context "5. Convert the numbers from step 4 back to letters" do
+
+        it "should have a method to return the step result" do
+          decrypt.should respond_to(:numbers_to_letters)
+          decrypt.numbers_to_letters([3, 15, 4, 5, 9, nil, 14, 18, 21, 2, 25, nil, 12, 9, 22, 5, 12, nil, 15, 14, 7, 5, 18]).should == "CODEI NRUBY LIVEL ONGER"
+        end
+
+      end
+
+      context "additional messages to decrypt" do
+
+        let(:decrypt){ Rubyquiz::Solitaire::Decrypt }
+
+        {
+            "CLEPK HHNIY CFPWH FDFEH" => "YOURC IPHER ISWOR KINGX",
+            "ABVAW LWZSY OORYK DUPVH" => "WELCO METOR UBYQU IZXXX"
+        }.each do |encrypted_message, decrypted_message|
+
+          it "should be able to decrypt the message: #{encrypted_message}" do
+
+            decrypt.run(encrypted_message).should == decrypted_message
+
+          end
+
         end
 
       end
